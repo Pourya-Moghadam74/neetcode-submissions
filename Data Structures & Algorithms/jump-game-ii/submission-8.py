55e511a7193ee5b29f513e46a7e5085852b0186f
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-05-22 15:50:16 -0400

    Add: jump-game-ii - submission-8

---
+++ Data Structures & Algorithms/jump-game-ii/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        cache = {}
+
+        def dfs(i, cache):
+            if i == len(nums) - 1:
+                return 0
+            
+            if i in cache:
+                return cache[i]
+            
+            res = float('inf')
+            for j in range(i + 1, min(len(nums), nums[i] + i + 1)):
+                res = min(res, 1 + dfs(j, cache))
+            
+            cache[i] = res
+            return res
+
+        return dfs(0, cache)
