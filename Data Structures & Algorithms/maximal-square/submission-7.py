d4b3974c42ef86b1341b910f569398b01e607145
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-06-07 03:25:35 +0000

    Add: maximal-square - submission-7

---
+++ Data Structures & Algorithms/maximal-square/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        cache = {}
+
+        def dfs(r, c):
+            if r >= m or c >= n:
+                return 0
+            
+            if (r, c) in cache:
+                return cache[(r, c)]
+
+            right = dfs(r, c + 1)
+            down = dfs(r + 1, c)
+            diag = dfs(r + 1, c + 1)
+
+            if matrix[r][c] == "1":
+                cache[(r, c)] = 1 + min(right, down, diag)
+            else:
+                cache[(r, c)] = 0
+
+            return cache[(r, c)]
+        
+        dfs(0, 0)
+        return max(cache.values()) ** 2
