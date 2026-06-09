558fb518bf4e0011085d3ae947f05b47c99df245
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-06-09 01:31:33 +0000

    Add: permutations-ii - submission-11

---
+++ Data Structures & Algorithms/permutations-ii/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        count = {}
+        res = []
+        path = []
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        
+        def dfs():
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            
+            for num in count:
+                if count[num] > 0:
+                    path.append(num)
+                    count[num] -= 1
+                    dfs()
+                    count[num] += 1
+                    path.pop()
+                
+        
+        dfs()
+
+        return res
