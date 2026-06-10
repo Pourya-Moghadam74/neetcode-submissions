af22833aee9415cb28cf7f21d96f53c33853de89
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-06-10 00:40:50 +0000

    Add: longest-common-subsequence - submission-23

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-23.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        cache = {}
+
+        def dfs(i, j):
+            if i >= m or j >= n:
+                return 0
+
+            if (i, j) in cache:
+                return cache[(i, j)]
+
+            if text1[i] == text2[j]:
+                cache[(i, j)] = 1 + dfs(i + 1, j + 1)
+                return cache[(i, j)]
+
+            cache[(i, j)] = max(dfs(i + 1, j), dfs(i, j + 1))
+            return cache[(i, j)]
+        
+        return dfs(0, 0)
