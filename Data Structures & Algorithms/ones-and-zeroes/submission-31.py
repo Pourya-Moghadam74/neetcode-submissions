36a25c756a05951fcabcbf39d0081d9087b2ea76
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-06-11 00:45:49 +0000

    Add: ones-and-zeroes - submission-31

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-31.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        cache = {}
+
+        def dfs(i, zerosCount, onesCount):
+            if zerosCount > m or onesCount > n:
+                return float('-inf')
+            
+            if i > len(strs):
+                return float('-inf')
+            
+            if i == len(strs):
+                return 0
+
+            if (i, zerosCount, onesCount) in cache:
+                return cache[(i, zerosCount, onesCount)]
+            
+            z = strs[i].count("0")
+            o = strs[i].count("1")
+
+            take = 1 + dfs(i + 1, zerosCount + z, onesCount + o)
+            skip = dfs(i + 1, zerosCount, onesCount)
+            cache[(i, zerosCount, onesCount)] = max(take, skip)
+
+            return cache[(i, zerosCount, onesCount)]
+
+        return dfs(0, 0, 0)
