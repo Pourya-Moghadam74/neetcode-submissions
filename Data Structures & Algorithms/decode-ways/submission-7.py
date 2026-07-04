26f0f47ebc4f01e1b1ae0c21b0f63ac556405c9a
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-07-04 21:22:15 +0000

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        cache = {}
+
+        def dfs(i):
+            if i == n:
+                return 1
+            
+            if s[i] == "0":
+                return 0
+
+            if i in cache:
+                return cache[i]    
+
+            res = dfs(i + 1)
+
+            if i < n - 1 and (s[i] == "1" or (s[i] == "2" and s[i + 1] < "7")):
+                res += dfs(i + 2)
+            
+            cache[i] = res
+            return cache[i]
+        
+        return dfs(0)
