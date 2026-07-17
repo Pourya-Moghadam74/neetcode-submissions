fc98fd760b21e33b502cb6b4a7e4f353cfe63fb0
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-07-17 01:59:55 +0000

    Add: palindrome-partitioning - submission-31

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-31.py
@@ -0,0 +1,32 @@
+class Solution:
+
+    def isPal(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        path = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(path[:])
+                return
+            
+            for j in range(i, len(s)):
+                if self.isPal(s, i, j):
+                    path.append(s[i: j + 1])
+                    dfs(j + 1)
+                    path.pop()
+        
+        dfs(0)
+
+        return res
+
+            
