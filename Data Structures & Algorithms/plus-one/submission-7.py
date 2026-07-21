65d937c01e058924506b4f5b498f23c705e35a55
Pourya Moghadam <135594559+Pourya-Moghadam74@users.noreply.github.com>
2026-07-21 04:05:49 +0000

    Add: plus-one - submission-7

---
+++ Data Structures & Algorithms/plus-one/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        one = [0] * n
+        one[-1] = 1
+        res = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            tmp = digits[i] + one[i] + res[i + 1]
+            res[i + 1] = tmp % 10
+            res[i] = tmp // 10
+
+        return res if res[0] != 0 else res[1:]
